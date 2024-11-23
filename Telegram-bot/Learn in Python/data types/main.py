445f29fffe0fc67e88b07c536c020550f550c514
Mark Arcedas <markarcedas27@gmail.com>
2024-11-23 21:53:30 +0800

    StringTesting

--- Telegram-bot/Learn in Python/data types/main.py
+++ Telegram-bot/Learn in Python/data types/main.py
@@ -2,3 +2,17 @@ myfloat = 9.0
 print(myfloat)
 myfloats = float(1)
 print(myfloats)
+
+
+# change this code
+mystring = "hello"
+myfloat = 10.0
+myint = 20
+
+# testing code
+if mystring == "hello":
+    print("String: %s" % mystring)
+if isinstance(myfloat, float) and myfloat == 10.0:
+    print("Float: %f" % myfloat)
+if isinstance(myint, int) and myint == 20:
+    print("Integer: %d" % myint)
